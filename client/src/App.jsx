9c8cb013ed3950e70002946eb97f5fe4c119d53f
FrancoRegg <francoreggiardo97@gmail.com>
2025-12-10 12:20:27 +0100

    Function and component for a group to access the waiting room

---
+++ client/src/App.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+// Conectar Backend
+const socket = io('http://localhost:3000');
+
+function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [inside, setInside] = useState(false);
+  const [nameGroup, setNameGroup] = useState("");
+
+  useEffect(() => {
+    // Escuchar eventos de conexión del socket
+    socket.on('connect', () => {
+      setIsConnected(true);
+      console.log("Conectado al servidor con ID:", socket.id);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    // Limpieza al cerrar el componente
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+    };
+  }, []);
+
+  function enterGame(){
+    
+    socket.emit('join_game', {name:nameGroup})
+    setInside(true);
+  }
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}>
+      <h1>Join the Game!</h1>
+      
+      {inside ? `BIENVENIDO AL JUEGO EQUIPO ${nameGroup.toUpperCase()}` : 
+      <div>
+        <label>
+          <input name="Entrada" value={nameGroup} onChange={e => setNameGroup(e.target.value)} />
+        </label>
+        <button onClick={enterGame}> Entrar al Juego </button>
+      </div>}
+      
+
+
+      <h6>
+        Estado del Servidor: {' '}
+        <span style={{ color: isConnected ? 'green' : 'red', fontWeight: 'bold' }}>
+          {isConnected ? '🟢 CONECTADO' : '🔴 DESCONECTADO'}
+        </span>
+      </h6>
+    </div>
+  );
+}
+
+export default App;
