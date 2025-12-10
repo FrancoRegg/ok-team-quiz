14054c7d67e6ccce9bcafc9ad19bf596550986db
FrancoRegg <francoreggiardo97@gmail.com>
2025-12-10 13:59:18 +0100

    Connected Group Rendering

---
+++ client/src/HostView.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
+
+function HostView() {
+    const [groups, setGroups] = useState([])
+
+
+    useEffect(() => {
+        socket.emit('join_game', {name:'HOST'})
+        socket.on('update_players', (data) =>{
+            setGroups(data)
+        })
+    }, []);
+
+    return(
+        <div>
+            <h1>Pantalla Grande</h1>
+            <ul>
+                {groups.filter(grupo => grupo.name !== 'HOST').map((grupo, i)=>(
+                    <li key={i}>{grupo.name}</li>
+                ))}
+            </ul>
+        </div>
+        
+    );
+};
+
+export default HostView;
