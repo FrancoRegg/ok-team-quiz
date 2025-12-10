f8e60e9ae2ec3dbacc3294c87c08925748c03d05
FrancoRegg <francoreggiardo97@gmail.com>
2025-12-10 12:17:03 +0100

    Event to send groups to the game room

---
+++ server/src/server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const http = require('http');
+const { Server } = require('socket.io')
+const cors = require('cors');
+
+const app = express() // Inicializar express
+
+app.use(cors()); // Permite la conexion desde el frontend
+
+const server = http.createServer(app); // Creamos el servidor HTTP a partir de Express
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173", // Esta es la URL donde correrá React
+        methods: ["GET", "POST"] 
+    }
+});
+
+const players = {}
+
+// Escucha los eventos de conexion
+io.on("connection", (socket) => {
+    console.log("Nueva conexion: ", socket.id);
+    socket.on('join_game', (data)=>{
+        const groupId = data.name 
+        if (groupId == ""){
+            console.log("No se permiten campos vacios")
+            return;
+        } 
+        players[socket.id] = {
+            name : groupId,
+            score : 0,
+            id : socket.id
+        };
+
+        socket.join('game_room')
+
+        const playerList = Object.values(players)
+        io.to('game_room').emit('update_players', playerList)
+        console.log(`${groupId} se unio a la partida`)
+    })
+
+    socket.on('disconnect', () => {
+        console.log("Desconectado: ", socket.id);
+        delete players[socket.id]
+    });
+    
+});
+
+
+const PORT = 3000
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
