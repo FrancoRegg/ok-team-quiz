0e9a240cf3ccd89722e645fb4fbe160d2c13515b
FrancoRegg <francoreggiardo97@gmail.com>
2025-12-20 12:04:14 +0100

    Environment variables configuration

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const http = require('http');
 const { Server } = require('socket.io')
 const cors = require('cors');
+const { sequelize } = require('../config/db');
 
+const port = process.env.PORT;
 const app = express() // Inicializar express
 
 app.use(cors()); // Permite la conexion desde el frontend
-
 const server = http.createServer(app); // Creamos el servidor HTTP a partir de Express
+
+
 
 const io = new Server(server, {
     cors: {
@@ -142,7 +146,7 @@ io.on("connection", (socket) => {
 });
 
 
-const PORT = 3000
-server.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`)
+
+server.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`)
 })
