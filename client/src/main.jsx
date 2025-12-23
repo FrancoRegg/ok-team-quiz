958d34f55d79a881c50bcf8d1c1eab6375964608
FrancoRegg <francoreggiardo97@gmail.com>
2025-12-23 20:22:00 +0100

    Administrator access path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import HostView from './HostView.jsx'
+import AdminView from './AdminView.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />            
+      <Route path="/" element={<App />} />    
+      <Route path="/admin" element={<AdminView />}/>        
       <Route path="/host" element={<HostView />} />
     </Routes>
     </BrowserRouter>
