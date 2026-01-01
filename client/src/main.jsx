ef1fe937d5690703d4c656e6db37ae64af5ef71e
FrancoRegg <francoreggiardo97@gmail.com>
2026-01-01 18:07:34 +0100

    Admin access protection

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,18 @@ import './index.css'
 import App from './App.jsx'
 import HostView from './HostView.jsx'
 import AdminView from './AdminView.jsx'
+import AdminGuard from './AdminGuard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />    
-      <Route path="/admin" element={<AdminView />}/>        
+      <Route path="/admin" element={
+        <AdminGuard> 
+          <AdminView /> 
+        </AdminGuard>
+      }/>        
       <Route path="/host" element={<HostView />} />
     </Routes>
     </BrowserRouter>
