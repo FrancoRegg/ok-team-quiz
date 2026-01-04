9c12ca0d214591a532863a3450e60dd5dc3c0b09
FrancoRegg <francoreggiardo97@gmail.com>
2026-01-04 17:04:50 +0100

    Data control for handling CRUD and path access

--- server/controllers/question.controller.js
+++ server/controllers/question.controller.js
@@ -13,14 +13,45 @@ exports.createQuestion = async(req, res) => {
   try{
     const { title, type, options, correctIndex, mediaUrl } = req.body;
     if(!title || !options) {
-             return res.status(400).json({ message: 'Faltan datos obligatorios' });
-        }
-    const newQuestion = await Question.create({ title, type, options, correctIndex, mediaUrl });
+         return res.status(400).json({ message: 'Faltan datos obligatorios' });
+      }
+      const newQuestion = await Question.create({ 
+         title, 
+         type, 
+         options, 
+         correctIndex, 
+         mediaUrl 
+      });
     
     return res.status(201).json(newQuestion);
  }catch(error){
     return res.status(500).json({ message: 'Error al crear preguntas' });
  }  
+}
+
+exports.updateQuestion = async(req, res) => {
+   try{
+    const { id } = req.params
+    const { title, options, correctIndex, type, mediaUrl } = req.body;
+
+    const question = await Question.findByPk(id);
+    if (!question) {
+        return res.status(404).json({ error: 'Pregunta no encontrada' });
+    }
+
+    await question.update({
+         title,
+         options,
+         correctIndex,
+         type,
+         mediaUrl
+    })
+
+    res.status(200).json(question)
+   }catch(error){
+      console.error(error);
+      return res.status(500).json({ message: 'Error al actualizar preguntas' });
+   }  
 }
 
 exports.deleteQuestion = async(req, res) => {
--- server/routes/questionRoutes.js
+++ server/routes/questionRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
-const { getQuestion, createQuestion, deleteQuestion } = require('../controllers/question.controller')
+const { getQuestion, createQuestion, updateQuestion, deleteQuestion } = require('../controllers/question.controller')
 
 router.get('/', getQuestion);
 router.post('/', createQuestion);
 router.delete('/:id', deleteQuestion);
+router.put('/:id', updateQuestion);
 
 module.exports = router;
