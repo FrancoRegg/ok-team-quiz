51b8da53c18c4f0796cdeadddeeaaaaf2878251f
FrancoRegg <francoreggiardo97@gmail.com>
2026-01-15 12:11:19 +0100

    Adjusted timing mode

--- server/config/sync.js
+++ server/config/sync.js
@@ -1,12 +1,12 @@
 const { sequelize } = require('./db');
-const { Question } = require('../models/Questions')
 
 async function sincro (){
 try{
-    await sequelize.sync();
-    console.log('Tablas sincronizadas correctamente');
+    await sequelize.sync( {alter: true} );
+    console.log('✅ Base de datos sincronizada, ALTER MODE');
 }catch(error){
-    console.error('Error al sincronizar las tablas', error);    
+    console.error('❌ Error al sincronizar BD', error);
+    throw error;    
 }};
 
 module.exports = { sincro }; 
