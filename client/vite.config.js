0fa890451c324f194d24cc5528be534624e0f0d2
FrancoRegg <francoreggiardo97@gmail.com>
2026-01-16 13:27:28 +0100

    Proxy settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,5 +7,11 @@ export default defineConfig({
   server: {
     host: true, // Expone el servidor a la red local
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
   }
 })
