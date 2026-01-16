9c630ddd6b7f3468623d297d37871c1652b68ff4
FrancoRegg <francoreggiardo97@gmail.com>
2026-01-16 12:40:26 +0100

    Typographical adjustment

--- server/config/sync.js
+++ server/config/sync.js
@@ -1,12 +1,13 @@
 const { sequelize } = require('./db');
 
 async function sincro (){
-try{
-    await sequelize.sync( {alter: true} );
-    console.log('✅ Base de datos sincronizada, ALTER MODE');
-}catch(error){
-    console.error('❌ Error al sincronizar BD', error);
-    throw error;    
-}};
+    try{
+        await sequelize.sync({alter: true});
+        console.log('✅ Base de datos sincronizada, ALTER MODE');
+    }catch(error){
+        console.error('❌ Error al sincronizar BD', error);
+        throw error;    
+    }
+};
 
 module.exports = { sincro }; 
