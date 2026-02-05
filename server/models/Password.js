c2f9834863d6c78a5a052ba6dbb750df9ca096b0
FrancoRegg <francoreggiardo97@gmail.com>
2026-02-05 12:33:47 +0100

    New password model with bcrypt hashing

---
+++ server/models/Password.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db');
+
+const Password = sequelize.define('Password', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    passwordHash: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        field: 'password_hash'
+    },
+    isDefault: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        field: 'is_default'
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        field: 'updated_at'
+    }
+}, {
+    tableName: 'passwords',
+    timestamps: false
+});
+
+module.exports = Password;
